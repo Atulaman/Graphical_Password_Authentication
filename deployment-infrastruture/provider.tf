bf7f4d30962977b23915b78718b566c3916e08e9
Atulaman <atul.aman@remotestate.com>
2024-05-23 19:19:37 +0530

    added service principle authentication

--- deployment-infrastruture/provider.tf
+++ deployment-infrastruture/provider.tf
@@ -14,4 +14,8 @@ provider "azurerm" {
   features {
 
   }
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subs_id
 }
---
+++ deployment-infrastruture/variables.tf
@@ -0,0 +1,12 @@
+variable "client_secret" {
+  default = "VIa8Q~3b7m5MyV8NY4hx3vlLsxc85vMhCtmZ~cQ_"
+}
+variable "client_id" {
+  default = "4a889f48-d1b8-4e8a-9495-97e95c8b8d35"
+}
+variable "tenant_id" {
+  default = "1e69cb8f-5131-427e-82a9-ed33f4e4a6b3"
+}
+variable "subs_id" {
+  default = "d532fd4c-2fbe-43aa-98b5-9a2e7d3a1b3f"
+}
